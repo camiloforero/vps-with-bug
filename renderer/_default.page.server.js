bf46bbfbf36c1912dcd5b379d02df5e921b0ff32
Camilo Forero <camilo.forero@hey.com>
2022-08-05 10:21:32 -0500

    make logo public

--- renderer/_default.page.server.js
+++ renderer/_default.page.server.js
@@ -1,7 +1,6 @@
 import { renderToString } from '@vue/server-renderer'
 import { escapeInject, dangerouslySkipEscape } from 'vite-plugin-ssr'
 import { createApp } from './app'
-import logoUrl from './logo.svg'
 
 export { render }
 // See https://vite-plugin-ssr.com/data-fetching
@@ -20,7 +19,7 @@ async function render(pageContext) {
     <html lang="en">
       <head>
         <meta charset="UTF-8" />
-        <link rel="icon" href="${logoUrl}" />
+        <link rel="icon" href="/logo.svg" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="${desc}" />
         <title>${title}</title>
